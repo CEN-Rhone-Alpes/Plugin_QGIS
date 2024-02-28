93a9062f8b0820ef2c7b47d169a6fa785aafa47a
Tom Laveille <120559974+tlaveille-CENRA@users.noreply.github.com>
2024-02-28 08:38:44 +0100

    Add files via upload

--- QGIS3.bat
+++ QGIS3.bat
@@ -11,7 +11,7 @@ ipconfig | findstr /R /C:"IPv4 Address" >>\\100.100.100.100\users\tlaveille\logQ
 echo END INSTALL Q3 >>\\100.100.100.100\users\tlaveille\logQGIS3\log%USERNAME%.txt
 echo START COPY PROFILES >>\\100.100.100.100\users\tlaveille\logQGIS3\log%USERNAME%.txt
 
-XCOPY \\100.100.100.100\bd_sig\z_QGIS\Formations\tests_oslandia_b\default %APPDATA%\QGIS\QGIS3\profiles\default\ /s /e >>\\100.100.100.100\users\tlaveille\logQGIS3\log%USERNAME%.txt
+XCOPY \\100.100.100.100\bd_sig\z_QGIS\Formations\tests_oslandia_b\default %APPDATA%\QGIS\QGIS3\profiles\default\ /s /e /y>>\\100.100.100.100\users\tlaveille\logQGIS3\log%USERNAME%.txt
 
 echo END COPY PROFILES >>\\100.100.100.100\users\tlaveille\logQGIS3\log%USERNAME%.txt
 echo START UNINSTALL QGIS 2 >>\\100.100.100.100\users\tlaveille\logQGIS3\log%USERNAME%.txt
